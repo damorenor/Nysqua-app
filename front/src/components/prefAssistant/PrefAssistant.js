27e55fdf752724181852ace1e6b19db121f8f079
Nicolas Saavedra <hnsaavedraa@unal.edu.co>
2019-09-29 15:43:37 -0500

    assistant structure

---
+++ front/src/components/prefAssistant/PrefAssistant.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+import './PrefAssistant.css';
+
+class PrefAssistant extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+        return()
+    }
+
+}
+
+export default PrefAssistant;
