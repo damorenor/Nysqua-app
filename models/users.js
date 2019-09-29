73e5aa34549e2aaf30c01ed53c35deabae370573
Jimmy Pulido <jiapulidoar@gmail.com>
2019-09-29 12:29:40 -0500

    Add token authentification

--- models/users.js
+++ models/users.js
@@ -1,12 +1,11 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-require('mongoose-currency').loadType(mongoose);
-var Currency = mongoose.Types.Currency;
+mongoose.set('useCreateIndex', true);
+const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
 
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
 
-var userSchema = new Schema({
+var UserSchema = new Schema({
     email: {
         type: String,
         require: true,
@@ -23,11 +22,11 @@ var userSchema = new Schema({
     },
     birthDate: {
         type: Date,
-        require: true
+        //require: true
     },
     gender: {
         type: String,
-        require: true
+       // require: true
     },
     biography: {
         type: String
@@ -48,20 +47,61 @@ var userSchema = new Schema({
     },
     magazineList: {
         type: [{ type: Schema.ObjectId, ref: 'Magazine' }]
-    }
+    },
+
+    tokens:[{
+        token:{
+            type:String,
+            required: true
+        }
+    }],
+    isAdmin: Boolean
 }, { timestamps: true }
 );
 
-userSchema.methods.encryptPassword = async (password) => {
-  const salt = await bcrypt.genSalt(saltRounds);
-  const hash = await bcrypt.hash(password, salt);
-  return hash;
-};
 
-userSchema.methods.checkPassword = async function (password) {
-  return await bcrypt.compare(password, this.password);
-};
 
-var User = mongoose.model('User', userSchema);
+UserSchema.statics.checkValidCredentials = async (email, password) => {
+    const user = await User.findOne({email})
+
+    if(!user){
+        throw new Error('Unable to login 2')
+    }
+    const isMatch = await bcrypt.compare(password,user.password)
+
+    if(!isMatch){
+        throw new Error('Unable to login 2')
+    }
+
+    return user
+}
+
+//custom method to generate authToken 
+UserSchema.methods.newAuthToken = async function(){
+    const user  = this
+    const token =  jwt.sign({ _id: user.id.toString() },'thisismynewblog', {expiresIn: "7 days"})
+    user.tokens = user.tokens.concat({ token })
+    await user.save()
+    return token
+}
+
+//hash the plain text password before saving
+UserSchema.pre('save', async function(next){
+    const user = this
+    if(user.isModified('password')){
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+    next()
+})
+
+UserSchema.pre('remove', async function(next){
+    const user = this
+    await Post.deleteMany({author: user._id})
+    next()
+})
+  
+
+var User = mongoose.model('User', UserSchema);
+
 
 module.exports = User;
