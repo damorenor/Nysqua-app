3c8b1c167778cb85f4f1d2129940b192c52502bd
Jimmy Pulido <jiapulidoar@gmail.com>
2019-10-12 10:04:01 -0500

    Add categories dic:sparkles:

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,6 +1,16 @@
 const JWT = require('jsonwebtoken');
 const User = require('../models/users');
 
+
+// dictonary with subcategories 
+const categories = {
+    "Hombre": ["Camisas", "Camisetas", "Pantalones", "Sweaters y Cardigans", "Bermudas", "Chaquetas y Blazers", "Zapatos", "Buzos", "Jeans", "Accesorios", "Pijamas"],
+    "Mujer": ["Blusas", "Zapatos", "Buzos", "Chalecos", "Jeans", "Camisetas", "Pantalones", "Shorts", "Vestidos y Faldas", "Chaquetas y Blazers", "Sweaters y Cardigans", "Accesorios", "Pijamas"],
+    "Ninos": ["Camisas", "Camisetas", "Pantalones", "Sweaters y Cardigans", "Bermudas", "Chaquetas y Blazers", "Zapatos", "Buzos", "Jeans", "Accesorios", "Pijamas"],
+    "ninas": ["Blusas", "Zapatos", "Buzos", "Chalecos", "Jeans", "Camisetas", "Pantalones", "Shorts", "Vestidos y Faldas", "Chaquetas y Blazers", "Sweaters y Cardigans", "Accesorios", "Pijamas"], 
+    "Bebes": ["Camisas", "Blusas", "Camisetas", "Pantalones", "Sweaters y Cardigans", "Bermudas", "Chaquetas y Blazers", "Zapatos", "Buzos", "Jeans", "Accesorios", "Pijamas ", "Shorts", "Vestidos y Faldas"]
+  };
+  
 module.exports = {
 
     googleOAuth: async (req, res, next) => {
