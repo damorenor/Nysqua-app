200b692f25794b55a2c4a4fdab7d5bb6797a3c75
Nicolas Saavedra <hnsaavedraa@unal.edu.co>
2019-10-18 18:29:12 -0500

    updating list elements and rate :lipstick:

--- front/src/components/userProfile/UserProfile.js
+++ front/src/components/userProfile/UserProfile.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import { withStyles, createMuiTheme } from '@material-ui/core/styles';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
+import { IconContext } from "react-icons";
+import StarRatings from 'react-star-ratings';
+import { FaPlus } from 'react-icons/fa';
 import SwipeableViews from 'react-swipeable-views';
 import ProductCard from './../productCard';
+
 import Navbar from '../home/Navbar';
 import './UserProfile.css';
 
@@ -18,13 +24,41 @@ class UserProfile extends Component {
       this.state={
           index : 0,    
           userData : {
-              biography: "texto de biografia",
-              username: "Lorem ipsum",
+              biography: "Texto de biografia",
+              username: "Nombre de Usuario",
+              rating : 3,
               exchangeList :[],
-              garmentList: [],
+              garmentList: ["prenda1","prenda2","prenda3","prenda4","prenda5","prenda6","prenda7","prenda8" ,"prenda9","prenda10","prenda11"     ],
               profilePhoto : "https://www.rogowaylaw.com/wp-content/uploads/Blank-Employee.jpg",
           }
+
       }
+      this.gradient = 'linear-gradient(136deg, rgb(242, 113, 33) 0%, rgb(233, 64, 87) 50%, rgb(138, 35, 135) 100%)';
+      this.StyledButton = withStyles({
+        root: {
+            backgroundImage: this.gradient,
+            fontFamily: 'Product Sans !important',
+            borderRadius: 3,
+            border: 0,
+            color: 'white',
+            height: 48,
+            padding: '0 30px',
+            boxShadow: '0 3px 5px 2px rgba(255, 255, 255, .3)',
+            margin: '1vh 0vw 1vh 0vh',
+            fontSize: '1.05rem',
+            transitionProperty: 'opacity',
+            transitionDuration: '0.1s',
+            '&:hover': {
+                opacity: 0.9,
+            },
+            '&:active': {
+                boxShadow: '0 3px 5px 2px rgba(255, 255, 255, .3)',
+            },
+        },
+        label: {
+            textTransform: 'capitalize',
+        },
+    })(Button);
      
     }
     handleChange = (event, value) => {
@@ -40,7 +74,37 @@ class UserProfile extends Component {
           index,
         });
       };
+ 
     render(){
+        var myElements = [];
+        var completes= this.state.userData.garmentList.length - (this.state.userData.garmentList.length %  4);
+        for(var i = 0; i < Math.floor(this.state.userData.garmentList.length/4) ; i++) {
+            myElements.push(
+                <Grid container 
+                        spacing={4}
+                        direction = "row"
+                        justify = "center"
+                        alignItems = "stretch"
+                        wrap = "nowrap" 
+                        className = "products_margin">
+
+                            <Grid item xs={3}>
+                                <ProductCard/>
+                            </Grid>
+                            <Grid item xs={3}>
+                                <ProductCard/>
+                            </Grid>
+                            <Grid item xs={3}>
+                                <ProductCard/>
+                            </Grid>
+                            <Grid item xs={3}>
+                                <ProductCard/>
+                            </Grid>
+                </Grid>
+            );
+        };
+
+
 
         return(
             <div className = "profile_container">
@@ -59,7 +123,15 @@ class UserProfile extends Component {
                             <Grid item xs={8}>
                                 <div className = "text_info">
                                     <p>{this.state.userData.username}</p>
+                                    <StarRatings
+                                    rating={this.state.userData.rating}
+                                    starRatedColor="black"
+                                    numberOfStars={5}
+                                    name='rating'
+                                    starDimension ="25px"
+                                    />
                                     <p>{this.state.userData.biography}</p>
+                                 
                                     <p>Intercambios</p> 
                                 </div>
                             </Grid>
@@ -73,46 +145,35 @@ class UserProfile extends Component {
                         </Tabs>
                         <SwipeableViews index={this.state.index} onChangeIndex={this.handleChangeIndex}>
                             <div className= "tab_garment">
-                                <Grid container 
-                                spacing={4}
-                                direction = "row"
+                                <Grid
+                                container 
+                                direction = "column"
                                 justify = "center"
-                                alignItems = "stretch"
-                                wrap = "nowrap" >
-                                    <Grid item xs={3}>
-                                        <ProductCard/>
-                                    </Grid>
-                                    <Grid item xs={3}>
-                                        <ProductCard/>
-                                    </Grid>
-                                    <Grid item xs={3}>
-                                        <ProductCard/>
-                                    </Grid>
-                                    <Grid item xs={3}>
-                                        <ProductCard/>
-                                    </Grid>
+                                alignItems = "center"
+                                wrap = "nowrap">
+                                <Grid item xs={4}>
+                                <this.StyledButton 
+									fullWidth
+									focusRipple
+									variant="contained"
+									size="medium"
+									text="bold"
+								>
+                                <IconContext.Provider value={{ className: 'button_icon' }}>
+                                    <div>
+                                    <FaPlus/> 
+                                    </div>
+                                </IconContext.Provider>
+                                  Añadir prenda
+							</this.StyledButton>    
                                 </Grid>
-                                <Grid container 
-                                spacing={4}
-                                direction = "row"
-                                justify = "center"
-                                alignItems = "stretch"
-                                wrap = "nowrap" 
-                                className = "products_margin">
+                                </Grid>
 
-                                    <Grid item xs={3}>
-                                        <ProductCard/>
-                                    </Grid>
-                                    <Grid item xs={3}>
-                                        <ProductCard/>
-                                    </Grid>
-                                    <Grid item xs={3}>
-                                        <ProductCard/>
-                                    </Grid>
-                                    <Grid item xs={3}>
-                                        <ProductCard/>
-                                    </Grid>
-                                </Grid>
+                                <div>
+                                {myElements}
+                            </div>
+                                
+                                
                             </div>
 
                             <div className= "tab_garment">Aca estaran los catalogos del usuario</div>
