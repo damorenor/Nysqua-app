9fde1458f1bf1a6a3c284f9915ba1e095417cedf
Nicolas Saavedra <hnsaavedraa@unal.edu.co>
2019-11-19 22:19:46 -0500

    finish categories bar :lipstick:

--- front/src/components/home/CategoriesBar.js
+++ front/src/components/home/CategoriesBar.js
@@ -1,29 +1,44 @@
-import React, { Component } from "react";
+    import React, { Component } from "react";
 import Grid from '@material-ui/core/Grid';
+import PopupMenu from './PopupMenu';
 import './CategoriesBar.css';
 
 class CategoriesBar extends Component {
 
     constructor(props) {
       super(props);
-      this.subcategories=[ {name:"Camisas"},
-      {name:"Camisetas"},
-      {name:"Pantalones"},
-      {name:"Sweaters y Cardigans"},
-      {name:"Bermudas"},
-      {name:"Chaquetas y Blazers"},
-      {name:"Zapatos"},
-      {name:"Buzos"},
-      {name:"Jeans"},
-      {name:"Accesorios"},
-      {name:"Pijamas"},
-      {name:"Blusas"},
-      {name:"Shorts"},
-      {name:"Vestidos y Faldas" },];
+      this.state = {
+ 
+          categories : {
+            "Hombre": ["Camisas", "Camisetas", "Pantalones", "Bermudas", "Zapatos", "Buzos", "Jeans", "Accesorios", "Pijamas", "Sweaters y Cardigans", "Chaquetas y Blazers"],
+            "Mujer": ["Blusas", "Zapatos", "Buzos", "Chalecos", "Jeans", "Camisetas", "Pantalones", "Shorts", "Vestidos y Faldas", "Chaquetas y Blazers", "Sweaters y Cardigans", "Accesorios", "Pijamas"],
+            "Ninos": ["Camisas", "Camisetas", "Pantalones", "Bermudas", "Zapatos", "Buzos", "Jeans", "Accesorios", "Pijamas", "Sweaters y Cardigans", "Chaquetas y Blazers"],
+            "Ninas": ["Blusas", "Zapatos", "Buzos", "Chalecos", "Jeans", "Camisetas", "Pantalones", "Shorts", "Vestidos y Faldas", "Chaquetas y Blazers", "Sweaters y Cardigans", "Accesorios", "Pijamas"],
+            "Bebes": ["Camisas", "Blusas", "Camisetas", "Pantalones", "Bermudas", "Zapatos", "Buzos", "Jeans", "Accesorios", "Pijamas ", "Shorts", "Vestidos y Faldas", "Sweaters y Cardigans", "Chaquetas y Blazers"]
+        },
+
+      }
+      
+     
     };
     
 
     render(){
+        const manSubcategories = this.state.categories.Hombre.map(function (item) {
+            return <li>< button onClick= {()=>{console.log("click")}} className= "Menu-item" type="button"><p>{item}</p></button></li>
+        });
+        const womanSubcategories = this.state.categories.Mujer.map(function (item) {
+            return <li>< button onClick= {()=>{console.log("click")}} className= "Menu-item" type="button"><p>{item}</p></button></li>
+        });
+        const boySubcategories = this.state.categories.Ninos.map(function (item) {
+            return <li>< button onClick= {()=>{console.log("click")}} className= "Menu-item" type="button"><p>{item}</p></button></li>
+        });
+        const girlSubcategories = this.state.categories.Ninas.map(function (item) {
+            return <li>< button onClick= {()=>{console.log("click")}} className= "Menu-item" type="button"><p>{item}</p></button></li>
+        });
+        const babySubcategories = this.state.categories.Bebes.map(function (item) {
+            return <li>< button onClick= {()=>{console.log("click")}} className= "Menu-item" type="button"><p>{item}</p></button></li>
+        });
         return(
             <div  className ="container-cat">
                 <Grid 
@@ -33,22 +48,62 @@ class CategoriesBar extends Component {
                    
                     alignItems="center"
                     wrap = "nowrap" >   
-                    
-                        <Grid item xs={2} >
-                        <p    href="/SignUp" > Hombre </p>
+
+                        <Grid item xs={2}>
+                            <PopupMenu id="PopupMenuUnder" menuLabel="Menu Under" title="Hombre " position="under">
+                                <div className ="MenuTitle">
+                                    <p>SUBCATEGORÍAS</p>
+                                    <hr></hr>
+                                </div>
+                                <ul className="Menu">
+                                {manSubcategories}
+                                </ul>
+                            </PopupMenu>
                         </Grid>
                         <Grid item xs={2}>
-                            <p>Mujer</p>
+                            <PopupMenu id="PopupMenuUnder" menuLabel="Menu Under" title="Mujer" position="under">
+                                <div className ="MenuTitle">
+                                    <p>SUBCATEGORÍAS</p>
+                                    <hr></hr>
+                                </div>
+                                <ul className="Menu">
+                                {womanSubcategories}
+                                </ul>
+                            </PopupMenu>
                         </Grid>
                         <Grid item xs={2}>
-                        <p>Niño</p>
+                        <PopupMenu id="PopupMenuUnder" menuLabel="Menu Under" title="Niño" position="under">
+                                <div className ="MenuTitle">
+                                    <p>SUBCATEGORÍAS</p>
+                                    <hr></hr>
+                                </div>
+                                <ul className="Menu">
+                                {boySubcategories}
+                                </ul>
+                            </PopupMenu>
                 
                         </Grid>
                         <Grid item xs={2}>
-                            <p>Niña</p>
+                        <PopupMenu id="PopupMenuUnder" menuLabel="Menu Under" title="Niña" position="under">
+                                <div className ="MenuTitle">
+                                    <p>SUBCATEGORÍAS</p>
+                                    <hr></hr>
+                                </div>
+                                <ul className="Menu">
+                                {girlSubcategories}
+                                </ul>
+                            </PopupMenu>
                         </Grid>
                         <Grid item xs={2}>
-                            <p>Bebes</p>
+                            <PopupMenu id="PopupMenuUnder" menuLabel="Menu Under" title="Bebes" position="under">
+                                <div className ="MenuTitle">
+                                    <p>SUBCATEGORÍAS</p>
+                                    <hr></hr>
+                                </div>
+                                <ul className="Menu">
+                                {babySubcategories}
+                                </ul>
+                            </PopupMenu>
                         </Grid>
 
                 </Grid>
