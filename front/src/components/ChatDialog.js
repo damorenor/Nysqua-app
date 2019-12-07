9355711a8fea3af1c4bb2fc096cc3e5693151320
Nicolas Saavedra <hnsaavedraa@unal.edu.co>
2019-12-07 13:12:03 -0500

    finish chat view and update typography :sparkles: :bento: :lipstick:

--- front/src/components/ChatDialog.js
+++ front/src/components/ChatDialog.js
@@ -22,11 +22,17 @@ class ChatDialog extends Component {
             chatDialog: "",
             token: this.props.token,
             exchangeID: this.props.exchangeID,
-            
-        
+            exchangeData: this.props.exchangeData,
+            userData: this.props.userData,
+            userId: "",
+            otherUserId: "",
+            userState: false,
+            otherUserState: false,
+            otherUserName: "",
         };
         let chatSwipe;
         this.handleToNext = this.handleToNext.bind(this);
+        this.handleToprevious = this.handleToprevious.bind(this);
         this.handleToCancel = this.handleToCancel.bind(this);
         this.handleToFinish = this.handleToFinish.bind(this);
        
@@ -34,10 +40,12 @@ class ChatDialog extends Component {
     handleToNext() {
         this.chatSwipe.next();
     }
+    handleToprevious() {
+        this.chatSwipe.prev();
+    }
 
     handleToCancel() {
-        //this.chatSwipe.prev();
-        // poner en cancelado
+
         const config = {
             headers: {
                 'authorization': this.state.token,
@@ -55,6 +63,35 @@ class ChatDialog extends Component {
                 }, (error) => {
                 console.log(error);
             });
+    }
+
+    componentDidMount(){
+        if(this.state.userData._id == this.state.exchangeData.idUserOne){
+            this.setState({
+                userId: this.state.exchangeData.idUserOne,
+                otherUserId: this.state.exchangeData.idUserTwo,
+                userState : this.state.exchangeData.completeUserOne,
+                otherUserState: this.state.exchangeData.completeUserTwo,
+               
+            });
+        }else{
+            this.setState({ 
+                userId: this.state.exchangeData.idUserTwo,
+                otherUserId: this.state.exchangeData.idUserOne,
+                userState : this.state.exchangeData.completeUserTwo,
+                otherUserState: this.state.exchangeData.completeUserOne,
+            });
+        }
+
+        if (this.props.user1Name == this.state.userData.username){
+            this.setState({
+                otherUserName: this.props.user2Name
+            });
+        }else{
+            this.setState({
+                otherUserName: this.props.user1Name
+            });
+        }
     }
 
     handleToFinish(){
@@ -82,7 +119,7 @@ class ChatDialog extends Component {
     
         
         render() {
-
+            console.log(this.state.exchangeData);
             return (
             <div className = "chat_dialog_main_container">
                  <ReactSwipe
@@ -92,14 +129,99 @@ class ChatDialog extends Component {
                         el => (this.chatSwipe = el)
                     } >
                     <div className="chat_content">
-                        
+                        <div class="container clearfix">
+                            <div class="chat">
+                                <div class="chat-header clearfix">
+                                    <img src={this.state.userData.profilePhoto} alt="avatar" />
 
-                        
-                        <div className="chat_btn" onClick={this.handleToNext}>
-                            <p>Finalizar Intercambio</p>
+                                    <div class="chat-about">
+                                            <div class="chat-with">{"Chat con " + this.state.otherUserName}</div>
+                                            <div class="chat-num-messages">Visto a las 10:32 AM</div>
+                                    </div>
+
+                                </div>
+
+                                <div class="chat-history">
+                                    <ul className ="chat-list">
+                                        <li class="clearfix">
+                                            <div class="message-data align-right">
+                                                <span class="message-data-time" >10:10 AM, Hoy</span> &nbsp; &nbsp;
+                                        <span class="message-data-name" >Tu</span>
+
+                                            </div>
+                                            <div class="message other-message float-right">
+                                                Hi Vincent, how are you? How is the project coming along?
+                                        </div>
+                                        </li>
+
+                                        <li>
+                                            <div class="message-data">
+                                                    <span class="message-data-name"> {this.state.otherUserName}</span>
+                                                <span class="message-data-time">10:12 AM, Hoy</span>
+                                            </div>
+                                            <div class="message my-message">
+                                                Are we meeting today? Project has been already finished and I have results to show you.
+                                        </div>
+                                        </li>
+
+                                        <li class="clearfix">
+                                            <div class="message-data align-right">
+                                                <span class="message-data-time" >10:14 AM, Hoy</span> &nbsp; &nbsp;
+                                        <span class="message-data-name" >Tu</span>
+
+                                            </div>
+                                            <div class="message other-message float-right">
+                                                Well I am not sure. The rest of the team is not here yet. Maybe in an hour or so? Have you faced any problems at the last phase of the project?
+                                        </div>
+                                        </li>
+
+                                        <li>
+                                            <div class="message-data">
+                                                    <span class="message-data-name">{this.state.otherUserName}</span>
+                                                <span class="message-data-time">10:20 AM, Hoy</span>
+                                            </div>
+                                            <div class="message my-message">
+                                                Actually everything was fine. I'm very excited to show this to our team.
+                                        </div>
+                                        </li>
+
+                                        <li>
+                                            <div class="message-data">
+                                                    <span class="message-data-name"> {this.state.otherUserName}</span>
+                                                <span class="message-data-time">10:31 AM, Hoy</span>
+                                            </div>
+                                        </li>
+
+                                            <li class="clearfix">
+                                                <div class="message-data align-right">
+                                                    <span class="message-data-time" >10:14 AM, Hoy</span> &nbsp; &nbsp;
+                                        <span class="message-data-name" >Tu</span>
+
+                                                </div>
+                                                <div class="message other-message float-right">
+                                                    Abuenotemecuidas
+                                        </div>
+                                            </li>
+
+                                    </ul>
+                                </div>
+                                <div class="chat-message clearfix">
+                                    <textarea name="message-to-send" id="message-to-send" placeholder="Escribe tu mensaje" rows="3"></textarea>
+                                    <button>Enviar mensaje</button>
+                                </div>
+
+                            </div> 
+                            <div className="chat_buttons_container">
+                                <div className="chat_btn" onClick={this.handleToCancel}>
+                                    <p>Cancelar Intercambio</p>
+                                </div>
+                                <div className="chat_btn" onClick={this.handleToNext}>
+                                    <p>Finalizar Intercambio</p>
+                                </div>
+                            </div>
                         </div>
-  
-                    </div>
+    
+                        </div>
                     <div className="chat_content">
                         <h1 className="chat_title">¿Estás seguro que deseas finalizar el intercambio?</h1>
                         <div className = "chat_info_container">
@@ -109,17 +231,20 @@ class ChatDialog extends Component {
                         <p className="chat_subtitle">Una vez se finalizado el intercambio el canal de chat será cerrado para ambos usuarios, así que es muy importante que solo 
                                                     finalices el intercambio cuando todo está en orden.</p>
 
-                        </div>f
+                        </div>
                         
                       
-                        <p className="chat_subtitle">#Nombre de Usuario aún no ha finalizado el intercambio, el intercambio no dejara de estar activo hasta que esto suceda. </p>
-                        <div className="chat_buttons_container">
-                            <div className="chat_btn" onClick={this.handleToCancel}>
+                        <p className="chat_subtitle">
+                                {(!this.state.otherUserState) ? this.state.otherUserName + " aún no ha finalizado el intercambio, el intercambio no dejara de estar activo hasta que esto suceda." : 
+                                this.state.otherUserName +" ya finalizo el intercambio, una vez tu lo finalices este quedara totalmente completo"}    
+                        </p>
+                            <div className={!this.state.userState ? "chat_buttons_container" : "chat_buttons_container_2"}>
+                                <div className="chat_btn" onClick={this.handleToprevious}>
                                 <p>Volver al Chat</p>
                             </div>
-                            <div className="chat_btn" onClick={this.handleToFinish}>
+                            {(this.state.userState && !this.state.otherUserState) ? <div></div>: <div className="chat_btn" onClick={this.handleToFinish}>
                                 <p>Finalizar Intercambio</p>
-                            </div>
+                            </div>}
                         </div>
                     </div>
                     
--- front/src/components/home/Banner.js
+++ front/src/components/home/Banner.js
@@ -14,34 +14,41 @@ class Banner extends Component {
         
         this.content = [
             {
-              title: 'Project Name 1',
+              title: 'Chaquetas',
               description:
               'A Simple Project Description.',
               button: 'Read More',
-              image: 'https://i.imgur.com/ZXBtVw7.jpg',
+                image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cazadora-piel-marron-hombre-otono-2018-1535370632.jpg',
               user: 'Luan Gjokaj',
               userProfile: 'https://i.imgur.com/JSW6mEk.png'
             },
             {
-              title: 'Project Name 2',
+              title: 'Jeans',
               description:
               'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui.',
               button: 'Discover',
-              image: 'https://i.imgur.com/DCdBXcq.jpg',
+              image: 'https://s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2017/11/07141617/jean-ss.jpg',
               user: 'Erich Behrens',
               userProfile: 'https://i.imgur.com/0Clfnu7.png'
             },
             {
-              title: 'Project Name 3',
+              title: 'Camisetas',
               description:
               'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.',
               button: 'Buy now',    
-              image: 'https://i.imgur.com/DvmN8Hx.jpg',
+                image: 'https://www.cleanipedia.com/images/v2/c908487e8858ced5dac05db7094af8fa-1800w-1200h.jpg',
               user: 'Bruno Vizovskyy',
               userProfile: 'https://i.imgur.com/4KeKvtH.png'
             }
           ]
+          this.handleToClick = this.handleToClick.bind(this);
     }
+
+    handleToClick(title){
+        console.log("funciona");
+
+    }
+    
 
 
     render(){
@@ -54,9 +61,10 @@ class Banner extends Component {
                     className="sliderContent"
                     key={index}
                     style={{ background: `url('${item.image}') no-repeat center center` }}
+                   
                     >
-                        <div className="carousel-overlay"> 
-                            <div className ="TextCarousel">
+                        <div className="carousel-overlay" > 
+                            <div className ="TextCarousel"   >
                                 <Grid 
                                     container spacing={3}
                                     direction = "row"   
@@ -71,9 +79,6 @@ class Banner extends Component {
                                     </div>
                                     </Grid>
                                     <Grid item xs={4}>
-                                        {/* <button>{item.button}</button> */}
-
-                                        {/* boton */}
                                     </Grid>
 
                                 </Grid>
